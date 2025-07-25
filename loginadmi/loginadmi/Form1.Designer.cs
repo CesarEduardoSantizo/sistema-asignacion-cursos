c1c2839fedf3e213fee186c6793d55fa9d1483a2
CesarEduardoSantizo <santizosantosf37@gmail.com>
2025-07-24 20:16:08 -0600

    login administrador cesar santizo

---
+++ loginadmi/loginadmi/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace loginadmi
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_contraseña = new System.Windows.Forms.Label();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(7)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.txt_contraseña);
+            this.panel1.Controls.Add(this.lbl_contraseña);
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txt_usuario);
+            this.panel1.Controls.Add(this.lbl_usuario);
+            this.panel1.Location = new System.Drawing.Point(393, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 400);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(119, -15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_usuario
+            // 
+            this.lbl_usuario.AutoSize = true;
+            this.lbl_usuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_usuario.Location = new System.Drawing.Point(35, 159);
+            this.lbl_usuario.Name = "lbl_usuario";
+            this.lbl_usuario.Size = new System.Drawing.Size(55, 16);
+            this.lbl_usuario.TabIndex = 2;
+            this.lbl_usuario.Text = "Usuario";
+            this.lbl_usuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(38, 187);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(247, 22);
+            this.txt_usuario.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(102, 347);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(141, 26);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // lbl_contraseña
+            // 
+            this.lbl_contraseña.AutoSize = true;
+            this.lbl_contraseña.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_contraseña.Location = new System.Drawing.Point(35, 231);
+            this.lbl_contraseña.Name = "lbl_contraseña";
+            this.lbl_contraseña.Size = new System.Drawing.Size(77, 16);
+            this.lbl_contraseña.TabIndex = 5;
+            this.lbl_contraseña.Text = "Contraseña";
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(38, 250);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(247, 22);
+            this.txt_contraseña.TabIndex = 6;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(7)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frm_login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox txt_contraseña;
+        private System.Windows.Forms.Label lbl_contraseña;
+    }
+}
+
