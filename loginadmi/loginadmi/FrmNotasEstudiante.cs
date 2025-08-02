4a16ec918ebc09d011b8f626875c6956d6b96fcb
FernandoCahuex <fcahuexg@miumg.edu.gt>
2025-08-01 22:25:00 -0600

    Intento de error

---
+++ loginadmi/loginadmi/FrmNotasEstudiante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginadmi
+{
+    public partial class FrmNotasEstudiante : Form
+    {
+        public FrmNotasEstudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNotas_Click(object sender, EventArgs e)
+        {
+            FrmNotasEstudiante nuevoFormulario = new FrmNotasEstudiante();
+            nuevoFormulario.Show();
+            this.Hide();
+        }
+
+        private void btnPensum_Click(object sender, EventArgs e)
+        {
+            FrmPensum nuevoFormulario = new FrmPensum();
+            nuevoFormulario.Show();
+            this.Hide();
+        }
+    }
+}
